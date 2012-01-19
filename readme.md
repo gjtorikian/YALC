82078ea9c4599dcec716be46f4d762e1b8491a29
Garen Torikian <gjtorikian@gmail.com>
2012-01-18 22:51:46 -0800

    Support case sensitivity

--- readme.md
+++ readme.md
@@ -6,8 +6,10 @@ I got tired of looking for a broken link checker for my HTML files that existed 
 
 Super simple: just run
 
-	perl yalc.pl [input_dir]
+	perl yalc.pl _input_dir_ _[caseCheck]_
 
 And that's it. YALC will recurse through all _.html_ and _.htm_ files in _[input_dir]_ and subdirectories, examining any anchor tag (`<a>`) that doesn't go out to the web (therefore, local files only). If the file doesn't exist, it'll print a message.
+
+If you want to check case-sensitivity with the links, just pass the letter `y` as a second argument.
 
 The link checker also checks hash references. For example, `<a href="foo.html#bar">`. Even if _foo.html_ exists, if the `bar` reference does not, there will be a complaint.
--- yalc.pl
+++ yalc.pl
@@ -5,6 +5,7 @@ use File::Find;
 use Cwd;
 
 $dirPath = $ARGV[0];
+$caseCheck = $ARGV[1];
 
 my $file;
 my $currDir;
@@ -41,19 +42,39 @@ sub getAnchors {
 		my $href = $attr->{ href };
 		my $filepath = $currDir . "/" . $href;
 
-		if ($href !~ m/www/ && $href !~ m/http:/ && $href !~ m/https:/) # not an external link
+		# not an external link, or ignorable link (/,, #)
+		if ($href !~ m/www/ && $href !~ m/http:/ && $href !~ m/https:/ && $href ne '/' && $href ne '' && $href ne '#') 
 		{
-			if ($href =~ m/\#/) # a reference to somewhere
+			if ($href =~ m/\#/) # a reference to somewhere internal
 			{
 				my $hashFile = substr($href, 0, index($href, "#"));
 				$hashId = substr($href, index($href, "#") + 1); 
+
+				$filepath = $currDir . "/" . $hashFile;
+
+				# check preceeding file first
+				if ($caseCheck eq 'y')
+				{
+					casedFileCheck($hashFile, $file, $filepath);
+				}
+				else
+				{
+					uncasedFileCheck($hashFile);
+	 			}
+
+	 			# then validate hash
 				$h->parse_file($file);
 			}
 			else 
 			{
-				unless (-e $filepath) {
-	 				print $file . " is trying to incorrectly link to " . $href . " as $filepath : $text\n\n";
-	 			} 
+				if ($caseCheck eq 'y')
+				{
+					casedFileCheck($href, $file, $filepath);
+				}
+				else
+				{
+					uncasedFileCheck($filepath);
+	 			}
 	 		}
 	 	}
 	}
@@ -78,4 +99,44 @@ sub findHashes {
 			print $file . " has an incorrect hash to $hashId\n"; 
 		}	
 	}
+}
+
+sub casedFileCheck
+{
+	my ($href, $file, $filepath) = @_;
+					$found = 0;
+					$lastSlashPos = rindex($href, "/");
+					$refDirName = substr($href, 0, $lastSlashPos);
+					$refFileName = substr($href, $lastSlashPos + 1);
+
+					if ($refDirName eq '' || $lastSlashPos < 0)
+					{
+						$refDirName = "./";
+					}
+
+					opendir my($dh), $refDirName or die "Couldn't open dir '$refDirName' (href is $href, lastSlashPos is $lastSlashPos) : $!";
+					my @refFiles = readdir $dh;
+					closedir $dh;
+
+					foreach $refFile (@refFiles)
+					{
+						if ($refFile eq $refFileName)
+						{
+							$found = 1;
+						}
+					}
+
+					if ($found == 0 && $href ne '') 
+					{
+						print $file . " is trying to incorrectly link to " . $href . " as $filepath : $text\n\n";
+					}	
+}
+
+sub uncasedFileCheck
+{
+	my ($filepath) = @_;
+
+					unless (-e $filepath) {
+		 				print $file . " is trying to incorrectly link to " . $href . " as $filepath : $text\n\n";
+		 			} 
 }
